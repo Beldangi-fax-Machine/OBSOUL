bb13514784fe036721ba9e432c7ce92f2c07f623
Beldangi-fax-Machine <145410230+Beldangi-fax-Machine@users.noreply.github.com>
2023-09-19 13:34:36 -0400

    Update java.js

--- java.js
+++ java.js
@@ -15,26 +15,3 @@ function createStar() {
 
 setInterval(createStar, 500);
 
-// Get the footer element by its id
-const footer = document.getElementById("myFooter");
-
-let prevScrollpos = window.pageYOffset;
-
-// Listen for scroll events
-window.addEventListener("scroll", () => {
-  // Get the current scroll position
-  const currentScrollPos = window.pageYOffset;
-
-  // If the user is scrolling up (previous scroll position is greater than current scroll position)
-  if (prevScrollpos > currentScrollPos) {
-    // Show the footer
-    footer.style.display = "block";
-  } else {
-    // Hide the footer
-    footer.style.display = "none";
-  }
-  
-  // Update the previous scroll position
-  prevScrollpos = currentScrollPos;
-});
-
